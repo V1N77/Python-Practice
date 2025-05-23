cc324c8e4ba67dc585a23edb9de0adf9d146757e
V1N77 <159728348+V1N77@users.noreply.github.com>
2025-05-23 13:04:13 +1200

    Update Milstone_2_Code.py

--- Milestone_2/Milstone_2_Code.py
+++ Milestone_2/Milstone_2_Code.py
@@ -1,22 +1,23 @@
-#type password
-password = input ("Please create your password...  ")
+password = input("Please create your password...  ")
 
 import re
 
 if len(password) < 15:
- print("Password short, perhaps increase the length to atleast 15")
+    print("Password short, perhaps increase the length to at least 15")
 
 elif not re.search(r"[A-Z]", password):
- print("Password weak, perhaps add a Capital Letter!")
+    print("Password weak, perhaps add a Capital Letter!")
 
-elif not re.search(r"[1234567890]", password):
- print("Password weak, perhaps add a Number!")
+elif not re.search(r"[0-9]", password):
+    print("Password weak, perhaps add a Number!")
 
 elif not re.search(r"[{}/|;:',.<>?~`!@#$%^&*()]", password):
- print("Password weak, perhaps add a Special Character")
-
-elif not re.search(r"[]", password):
- print("Password weak, perhaps all digits must add up to number of letters in your password")
+    print("Password weak, perhaps add a Special Character")
 
 else:
- print("Password Strong")
+    digit_sum = sum(int(digit) for digit in password if digit.isdigit())
+    letter_count = sum(1 for char in password if char.isalpha())
+    if digit_sum != letter_count:
+        print("Password weak, perhaps all digits must add up to the number of letters in your password")
+    else:
+        print("Password Strong")
