97e1fc2bc735fa40f1c23abeda514f961a233514
V1N77 <159728348+V1N77@users.noreply.github.com>
2025-05-27 12:30:59 +1200

    upadate nearly done yesses

--- Milestone_2/Milstone_2_Code.py
+++ Milestone_2/Milstone_2_Code.py
@@ -1,9 +1,13 @@
 password = input("Please create your password...  ")
-
+#import re for re.search later
 import re
+#pie digits for later
+pi_digits = "3141592"
 
-if len(password) < 15:
-    print("Password short, perhaps increase the length to at least 15")
+face = "(._.)","(*O*)","(-_-)"
+
+if len(password) < 8:
+    print("Password short, perhaps increase the length to at least 8")
 
 elif not re.search(r"[A-Z]", password):
     print("Password weak, perhaps add a Capital Letter!")
@@ -19,5 +23,12 @@ else:
     letter_count = sum(1 for char in password if char.isalpha())
     if digit_sum != letter_count:
         print("Password weak, perhaps all digits must add up to the number of letters in your password")
+
+    elif pi_digits not in password:
+        print("Password weak, perhaps contain the first 7 digits of pie in your password")
+    
+    elif face not in password:
+        print("Password weak, perhaps contain one of these cute faces, (*O*),(._.), (-_-)")
+
     else:
         print("Password Strong")
