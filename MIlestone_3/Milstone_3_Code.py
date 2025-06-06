81107eb806394715924748739c98af1160689a22
V1N77 <159728348+V1N77@users.noreply.github.com>
2025-06-06 13:04:26 +1200

    Update Milstone_3_Code.py

--- MIlestone_3/Milstone_3_Code.py
+++ MIlestone_3/Milstone_3_Code.py
@@ -1,8 +1,31 @@
 import random
+score = 0
+imp = ''
+questions = [
+    {
+        "question": "What is the output of print(2 ** 3)?",
+        "options": ["5", "6", "8", "9"],
+        "fail_message": "nice",
+        "pass_message": "epic sigma",
+    },
+    {
+        "question": "Which keyword is used to create a function in Python?",
+        "options": ["func", "define", "def", "function"],
+        "fail_message": "nice",
+        "pass_message": "epic sigma",
+    },
+    {
+        "question": "What is the output of len('hello')?",
+        "options": ["4", "5", "6", "Error"],
+        "fail_message": "nice",
+        "pass_message": "epic sigma",
+    }
+]
 
-print("Welcome to the Quiz quiz.    The quiz all about quiz")
-
-questions = (("What does the fox say?")
-             ("")
-             ("")
-)
+while (True):
+    print(questions[score]["question"])
+    print('A:'+ questions[score]["options"][0])
+    print('B:'+ questions[score]["options"][1])
+    print('C:'+ questions[score]["options"][2])
+    print('D:'+ questions[score]["options"][3])
+    imp = input()
